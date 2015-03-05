e51736c21773d3186f92f06c2c2cbd1a24f04c6f
Yaroslav <saturn4er@gmail.com>
2015-03-05 17:08:23 +0200

    Added return explanation for modules

--- core/modules/module_skeleton/index.py
+++ core/modules/module_skeleton/index.py
@@ -1,3 +1,15 @@
 def build(params):
-    print(params)
+    return {
+        "libs": {
+            "x64": {
+                "debug": "realpath_to_debug_x64_lib",
+                "release": "realpath_to_release_x32_lib",
+            },
+            "x32": {
+                "debug": "realpath_to_debug_x32_lib",
+                "release": "realpath_to_release_x32_lib",
+            },
+        },
+        "headers": ["first headers folder", "second header folder"]  # could be a string
+    }
 
