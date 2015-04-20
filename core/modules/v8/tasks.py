95e8d319fb5716329180069417c52c048db4ea4e
OTER <maxymhammer@yandex.ru>
2015-04-20 21:54:43 +0300

    Working on v8 module

--- core/modules/v8/tasks.py
+++ core/modules/v8/tasks.py
@@ -6,11 +6,15 @@ from core.Dependencies.Tasks import check_param
 def generate_v8_project(module_name, task_params, module_params, result):
     # exec_path = os.path.abspath(check_param(module_name, task_params, 'exec_path'))
     old_path = os.path.abspath(".")
-    os.chdir("Lib/")
-    os.environ["PATH"] += ";" + old_path + "/Tools/depot_tools"
+    depot_tools_path = old_path + "/Lib/depot_tools"
+    os.chdir(depot_tools_path)
+    os.environ["PATH"] += ";" + depot_tools_path
 
     gyp_setup_log = open("gyp_setup.log", "w")
-    subprocess.call(["D:/Development/CppDepManager/core/modules/v8/Tools/depot_tools/python276_bin/python.exe", "build/gyp/setup.py"], stderr=gyp_setup_log, stdout=gyp_setup_log, shell=True)
+    # subprocess.call(["D:/Development/CppDepManager/core/modules/v8/Tools/depot_tools/python276_bin/python.exe", "build/gyp/setup.py"], stderr=gyp_setup_log, stdout=gyp_setup_log, shell=True)
+
+    subprocess.call(["fetch", "v8"], stderr=gyp_setup_log, stdout=gyp_setup_log, shell=True)
+
     gyp_setup_log.close()
 
     # gclient_log_file = open("gclient.log", "w")
@@ -20,12 +24,12 @@ def generate_v8_project(module_name, task_params, module_params, result):
 
 
 
-    script_path = "build/gyp_v8"
-    if os.path.exists(script_path):
-        log_file = open("gyp.log", "w")
-        subprocess.call(["D:/Development/CppDepManager/core/modules/v8/Tools/depot_tools/python276_bin/python.exe", script_path], stderr=log_file, stdout=log_file, shell=True)
-        log_file.close()
-    else:
-        raise Exception('Cannot find downloaded Qt VS addin."')
-        sys.exit(5)
+    # script_path = "build/gyp_v8"
+    # if os.path.exists(script_path):
+    #     log_file = open("gyp.log", "w")
+    #     subprocess.call(["D:/Development/CppDepManager/core/modules/v8/Tools/depot_tools/python276_bin/python.exe", script_path], stderr=log_file, stdout=log_file, shell=True)
+    #     log_file.close()
+    # else:
+    #     raise Exception('Cannot find downloaded Qt VS addin."')
+    #     sys.exit(5)
     os.chdir(old_path)
--- core/modules/v8/tasks_list.py
+++ core/modules/v8/tasks_list.py
@@ -6,15 +6,15 @@ download_path = os.path.join(directories["downloadDir"], 'depot_tools.zip')
 gyp_path = directories["libFolder"] + "/build/gyp"
 tasks = [
     {"task": "check_dependencies", "programs": ['git'], "params": ("version", 'rebuild')},
-    {"task": "git_clone", "repository": "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-     "sources_dir": v8_path},
+    # {"task": "git_clone", "repository": "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
+    #  "sources_dir": v8_path},
     # {"task": "git_checkout", "sources_dir": v8_path, "branch": "{version}"},
     # {"task": "git_clone", "repository": "https://chromium.googlesource.com/external/gyp/{}", "sources_dir": gyp_path},
     # {"task": "download_file",
     #  "destination": download_path,
     #  "url": "https://src.chromium.org/svn/trunk/tools/depot_tools.zip"},
-    # {"task": "un_7_zip", "file_location": download_path, "destination": 'Tools'},
-    # {"task": "generate_v8_project", "user_task": True},
+    # {"task": "un_7_zip", "file_location": download_path, "destination": v8_path},
+    {"task": "generate_v8_project", "user_task": True},
 
     # {"task": "add_location", "location": "eigen_sources/Eigen/src/"},
 ]
