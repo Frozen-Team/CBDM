5dca7bf6a5428e264357a3049ae1ad5bb1b54fad
OTER <maxymhammer@yandex.ru>
2015-09-03 23:13:17 +0300

    Add ignore_arrors strategy to git clone method

--- core/Tasks/vcs.py
+++ core/Tasks/vcs.py
@@ -7,11 +7,11 @@ from core.tools.git import Repo
 log_file = os.path.join(s_config.log_folder, 'git.txt')
 
 
-def git_clone(repo, repo_dir='', overwrite=True):
+def git_clone(repo, repo_dir='', overwrite=True, ignore_errors=False):
     print('Cloning {repo} to {repo_dir}'.format(repo=repo, repo_dir=repo_dir))
     repository = Repo(repo_dir, log_file)
     if repository.is_repo() and overwrite:
-        rmtree(repo_dir, ignore_errors=False)
+        rmtree(repo_dir, ignore_errors)
     repository.clone(repo)
 
 
